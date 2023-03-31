48f517907d32f2f88244a6effa9cc91cef7856e7
Theodore Wanner <wanner.theodore@gmail.com>
2023-03-31 16:48:52 +0200

    working model

--- public/index.js
+++ public/index.js
@@ -12,25 +12,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const timeMC = document.querySelector('#timeMC').value;
     const verbsMC = [...document.querySelector('#verbsMC').selectedOptions].map(option => option.value).join(", ");
 
+    //console.log(languageMC, gapsMC, levelMC, timeMC, verbsMC);
+        /*
     const data = {
-        prompt: `Generate a gap text in ${languageMC}, with ${gapsMC} gaps, and language level ${levelMC}. Use ${timeMC} and the following verbs: ${verbsMC}.`,
+        prompt: `Generate a gap text in ${languageMC}, with ${gapsMC} gaps, and language level ${levelMC}. Use ${timeMC} and the following verb: ${verbsMC}.`,
         max_tokens: 50,
         temperature: 0.7,
         n: 1,
         stop: "\n"
     };
+    */ 
 
-    const response = await fetch('/api/generate-gap-text', {
+
+
+    const data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": `Generate a gap text in ${languageMC}, with ${gapsMC} gaps, and language level ${levelMC}. Use ${timeMC} and the following verb: ${verbsMC}.`}],
+        "temperature": 0.7
+      }
+      
+      const response = await fetch('/api/generate-gap-text', {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
-    });
-    
-
-    const json = await response.json();
-    const solution = json.choices[0].text.trim();
+      });
+      
+      const json = await response.json();
+      const solution = json.choices && json.choices.length > 0 ? json.choices[0].message.content.trim() : 'No solution found.';
+      
 
     solutionText.textContent = `Solution: ${solution}`;
     });
--- src/server.js
+++ src/server.js
@@ -1,38 +1,46 @@
-const express = require('express');
-let fetch;
-(async () => {
-  fetch = await import('node-fetch').then(module => module.default);
-})();
+// Remove the following line
+// const fetch = require('node-fetch');
+require('dotenv').config();
 
+
+
+const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/gapText.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'gapText.html'));
+  });
+
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.post('/api/generate-gap-text', async (req, res) => {
-  const data = req.body;
-
-  try {
-    const fetch = (await import('node-fetch')).default;
-    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify(data),
-    });
-
-    const json = await response.json();
-    res.send(json);
-  } catch (error) {
-    res.status(500).send({ message: 'Error making request to OpenAI API' });
-  }
-});
+    const data = req.body;
+  
+    try {
+      const fetch = (await import('node-fetch')).default;
+      const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify(data),
+      });
+  
+      const json = await response.json();
+      res.send(json);
+    } catch (error) {
+      res.status(500).send({ message: 'Error making request to OpenAI API' });
+    }
+  });
+  
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
