76804beff4f4dd8481b11c8a3f37e8f0e2add787
Theodore Wanner <wanner.theodore@gmail.com>
2023-04-06 21:34:15 +0200

    New prompt, only german for now

--- public/gapText.js
+++ public/gapText.js
@@ -17,10 +17,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const data = {
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": `Schreibe einen Lückentext auf ${languageMC} mit genau ${gapsMC} Lücken (nicht mehr und nicht weniger) auf dem Sprachniveau ${levelMC}. Brauche die Zeitform ${timeMC}. In den Lücken sollte das konjugierte Verb ${verbsMC} in Einzahl oder Mehrzahl sein. Liste weiter unten sämtliche fehlenden konjugierten Verben in der richtigen Reihenfolge auf, welche den Text vervollständigen!`}],
+        "messages": [{"role": "user", "content": `Schreibe mir einen ${languageMC} Lückentext im untenstehenden Format. 
+        Der Lückentext soll genau ${gapsMC} Lücken haben, ist auf ${languageMC} geschrieben und die Lücken sind jeweils konjugierte Verben (nur die Verben ${verbsMC}, in Einzahl und Mehrzahl). 
+        Schreibe dem Text auf dem Sprachniveau ${levelMC} in der Zeitform ${timeMC}. 
+        Format:
+        Text: Ich ___(1) Geburtstag. Ich ___(2) 30 Jahre alt.
+        Lösung: 
+        (1) habe
+        (2) werde
+        `}],
         "temperature": 0.1
       }
-      
+     
       const response = await fetch('/api/generate-gap-text', {
         method: 'POST',
         headers: {
@@ -28,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         body: JSON.stringify(data),
       });
+      console.log(data);
       
       const json = await response.json();
       const solution = json.choices && json.choices.length > 0 ? json.choices[0].message.content.trim() : 'No solution found.';
