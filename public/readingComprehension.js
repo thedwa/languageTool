eecbeda84b3717f02c5ef5ccaf6d9eb4b7e3eef9
Theodore Wanner <wanner.theodore@gmail.com>
2023-04-07 13:20:12 +0200

    wip

--- public/readingComprehension.js
+++ public/readingComprehension.js
@@ -1,20 +1,17 @@
+// block text
+const formBlock = document.querySelector('.block-text-form');
+const solutionTextBlock = document.querySelector('.solution-text');
 
-    // block text
-    const formBlock = document.querySelector('.block-text-form');
-    const solutionTextBlock = document.querySelector('.solution-text');
-
-    if (formBlock) {
-        formBlock.addEventListener('submit', async (e) => {
+if (formBlock) {
+    formBlock.addEventListener('submit', async (e) => {
         e.preventDefault();
         setLoading(true); // Show the overlay and spinner
 
-
-    const languageMC = document.querySelector('#languageMC').value;
-    const lengthMC = document.querySelector('#lengthMC').value;
-    const levelMC = document.querySelector('#levelMC').value;
-    const timeMC = document.querySelector('#timeMC').value;
-    const topicInput = document.querySelector('#topicInput').value;
-
+        const languageMC = document.querySelector('#languageMC').value;
+        const lengthMC = document.querySelector('#lengthMC').value;
+        const levelMC = document.querySelector('#levelMC').value;
+        const timeMC = document.querySelector('#timeMC').value;
+        const topicInput = document.querySelector('#topicInput').value;
 
       const languagePrompts = {
         "German": `Verhalte dich wie ein Lehrer. Schreibe einen Text auf ${languageMC} mit ungefähr ${lengthMC} Wörter auf dem Sprachniveau ${levelMC}. Brauche die Zeitform ${timeMC}. Stelle dann 5 Fragen zum Inhalt vom Text. Dabei geht es um das Thema ${topicInput}`,
@@ -57,18 +54,58 @@
                                 </div>
                                 `;
        setLoading(false); // Hide the overlay and spinner
-    });
+          displayQuestionsWithInputs(solution); // Call the function to display questions with input fields and a button
+          });
+   }
+
+   function splitSolutionIntoTextAndQuestions(solution) {
+    const lines = solution.split('\n');
+    const text = lines.slice(0, -6).join('\n');
+    const questions = lines.slice(-5);
+    return [text, questions];
 }
 
+function displayQuestionsWithInputs(solution) {
+  const [text, questions] = splitSolutionIntoTextAndQuestions(solution);
+  const questionsWithInputs = questions.map((question, index) => {
+      return `<p>${question}<input type="text" class="answer-input" data-question-number="${index + 1}"></p>`;
+  }).join('');
+
+  solutionTextBlock.innerHTML = `
+      <div class="solution-block">
+          <h3>Text:</h3>
+          <p>${text.replace(/\n/g, '<br>')}</p>
+          <h3 id="questionsh3" >Questions:</h3>
+          ${questionsWithInputs}
+          <button class="smallButton">Submit Answers</button>
+      </div>
+  `;
+    // Add event listener to the 'submit answers' button
+    document.querySelector('.submit-answers').addEventListener('click', async () => {
+      const answerInputs = document.querySelectorAll('.answer-input');
+      const answers = Array.from(answerInputs).map(input => input.value);
+
+      // Send the text and answers to the OpenAI API for correction
+      const correctedAnswers = await sendAnswersForCorrection(text, questions, answers);
+
+      // Display corrected answers
+      const correctedAnswersHTML = correctedAnswers.map((answer, index) => `<p>${index + 1}. ${answer}</p>`).join('');
+      solutionTextBlock.insertAdjacentHTML('beforeend', `<div class="corrected-answers"><h3>Corrected Answers:</h3>${correctedAnswersHTML}</div>`);
+  });
+}
 
 
 
 //loader & spinner
 function setLoading(loading) {
-    const overlay = document.querySelector('.overlay');
-    if (loading) {
+  const overlay = document.querySelector('.overlay');
+  if (loading) {
       overlay.style.display = 'flex';
-    } else {
+  } else {
       overlay.style.display = 'none';
-    }
-  };
+  }
+}
+
+
+
+
